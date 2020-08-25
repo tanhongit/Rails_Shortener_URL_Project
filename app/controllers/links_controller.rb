84347169253df6b275ebab40dd2ed00818d24e42
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-25 18:01:56 +0700

    pry-rails gem and check info create link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,8 @@ class LinksController < ApplicationController
         # original_url = params[:link][:original_url]
         shortener = Shortener.new(links_params[:original_url])
         @link = shortener.generate_short_link
+        # # check info create link
+        # binding.pry
     end
 
     private
