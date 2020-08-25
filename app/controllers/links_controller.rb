d391456beceacbdd74880a101fc6ae4f3d19e09e
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-25 17:11:10 +0700

    test link controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,7 @@
 class LinksController < ApplicationController
     def create 
+        original_url = params[:link][:original_url]
+        shortener = Shortener.new(original_url)
+        @link = shortener.generate_short_link
     end
 end
--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe LinksController, type: :controller do
     it "can shortena link provided by a user" do
         url = "https://www.favoritewebsite.com/articles/how-to-cook"
         post :create, params: {link: {original_url: url}}
-        
-        
+        link = assigns(:link)
+        expect(link.original_url).to eq(url)
+        expect(link.valid?).to be(true)   
+        expect(link.lookup_code.length).to eq(7) 
     end
 end
