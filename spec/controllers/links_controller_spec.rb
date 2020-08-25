3161e991638cd1c6ce8f5b5b96deab5d3c557f45
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-25 17:24:42 +0700

    fix for test link controller

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
-    it "can shortena link provided by a user" do
+    it "can shorten link provided by a user" do
+        request.env["HTTP_ACCEPT"] = "text/javascript"
+
         url = "https://www.favoritewebsite.com/articles/how-to-cook"
-        headers = { "CONTENT_TYPE" => "text/javascript"}
-        post :create, params: {link: {original_url: url}}, headers: headers
+        post :create, params: {link: {original_url: url}}
         link = assigns(:link)
         expect(link.original_url).to eq(url)
         expect(link.valid?).to be(true)   
