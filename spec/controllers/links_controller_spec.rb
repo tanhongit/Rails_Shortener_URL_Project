f5c46ea2d4f2ef6e1105020d1c818cc274c1212c
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-25 17:27:13 +0700

    here link dot persisted

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe LinksController, type: :controller do
         expect(link.original_url).to eq(url)
         expect(link.valid?).to be(true)   
         expect(link.lookup_code.length).to eq(7) 
+
+        # here link dot persisted
+        expect(link.persisted?).to be(true) 
+
         expect(response).to render_template('create') 
     end
 end
