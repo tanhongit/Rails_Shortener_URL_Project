a087b558da227535675808de6c4ccedc96a8efc5
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-25 15:44:17 +0700

    create test for original URL and lookup code of Link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,13 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  
-    it "always has an original URL and short code" do
+
+    it "is valid if it has an original URL and lookup code" do
+        link = Link.new(
+            original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+            lookup_code: "1234567"
+        )
+        expect(link.valid?).to be(true)
+    end
+
+    it "is invalid if does not have lookup code" do
+        link = Link.new(
+            original_url: "https://www.favoritewebsite.com/articles/how-to-cook"
+        )
+        expect(link.valid?).to be(false)
+    end
+
+    it "is invalid if does not have original URL" do
+        link = Link.new(
+            lookup_code: "1234567"
+        )
+        expect(link.valid?).to be(false)
+    end
+
+    it "is invalid if the lookup code already exists" do
         link = Link.new(
             original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
             lookup_code: "1234567"
         )
         link.save
-    end
 
+        link_2 = Link.new(
+            original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+            lookup_code: "1234567"
+        )
+        expect(link_2.valid?).to be(false)
+    end
 end
