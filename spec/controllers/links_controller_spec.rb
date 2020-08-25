a2fddf4880a8ae444e5b7e232338e7f567dcffea
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-25 17:17:30 +0700

    create response views

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe LinksController, type: :controller do
     it "can shortena link provided by a user" do
         url = "https://www.favoritewebsite.com/articles/how-to-cook"
-        post :create, params: {link: {original_url: url}}
+        headers = { "CONTENT_TYPE" => "text/javascript"}
+        post :create, params: {link: {original_url: url}}, headers: headers
         link = assigns(:link)
         expect(link.original_url).to eq(url)
         expect(link.valid?).to be(true)   
         expect(link.lookup_code.length).to eq(7) 
+        expect(response).to render_template('create') 
     end
 end
