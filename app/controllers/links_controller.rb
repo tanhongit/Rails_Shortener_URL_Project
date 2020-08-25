279e26e264228021d3c4b0d2f7aff5041db28284
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-25 17:13:58 +0700

    create links_params method

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,13 @@
 class LinksController < ApplicationController
     def create 
-        original_url = params[:link][:original_url]
-        shortener = Shortener.new(original_url)
+        # original_url = params[:link][:original_url]
+        shortener = Shortener.new(links_params[:original_url])
         @link = shortener.generate_short_link
+    end
+
+    private
+
+    def links_params
+        params.require(:link).permit(:original_url)
     end
 end
