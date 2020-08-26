36237d813a4cd19a3e9638e07f2a3af015f21954
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-26 14:48:49 +0700

    fix test Link redirection

--- spec/requests/link_redirection_spec.rb
+++ spec/requests/link_redirection_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Link redirection", type: :request do
     it "redirects to the original URL for a given short link" do
-        url = "https:tanhongit.com"
+        url = "https://tanhongit.com"
         shortener = Shortener.new(url)
         link = shortener.generate_short_link
 
