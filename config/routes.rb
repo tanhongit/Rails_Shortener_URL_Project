af460951d6d9fe23d5c3df628c98c09c148127c8
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-27 10:16:35 +0700

    add bootstrap and change devise layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, class_name: "User", module: :devise
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/links" => 'links#create'
