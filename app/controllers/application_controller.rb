8f5e6b925ae5feba40795e9e76ba9ff39a21c644
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-28 14:05:22 +0700

    fix homepage authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :authenticate_user!
     
     rescue_from CanCan::AccessDenied do |exception|
         render :file => "static/403.html", :status => 403, :layout => false
