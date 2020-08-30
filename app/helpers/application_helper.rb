6cf5e7e9a90a4d4687e25c00211c427ff0173c27
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-08-31 00:54:20 +0700

    change menu nav layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def active(path)
         current_page?(path) ? 'active' : ''
     end
+
+    def active_admin(path)
+        current_page?(path) ? 'active btn btn-primary' : 'btn btn-light'
+    end
 end
